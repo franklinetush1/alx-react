b4254e98819bd9d9be51cb33b5061bbb541cb83f
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-11-02 13:51:02 +0300

    Update index.js

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -7,15 +7,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
     <div className="root-notifications">
       <Notifications />
     </div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
